9e5d4e3c3ce21588c6624a28adeaf1534c190495
soraya-ire <rujiangkongshi@gmail.com>
2026-04-21 13:22:46 +0900

    Initial Commit

---
+++ HikingSurvey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HikingSurvey
+//
+//  Created by soraya irie on 2026/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by soraya irie on 2026/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
