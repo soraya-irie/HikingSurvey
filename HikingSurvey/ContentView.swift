0a5a114f72cf33622f4b7a3cdc3c0662958adff9
soraya-ire <rujiangkongshi@gmail.com>
2026-04-21 13:26:56 +0900

    Complete Section2 Step1 - analyze sentiment in text

--- HikingSurvey/ContentView.swift
+++ HikingSurvey/ContentView.swift
@@ -1,19 +1,12 @@
-//
-//  ContentView.swift
-//  HikingSurvey
-//
-//  Created by soraya irie on 2026/04/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Opinions on Hiking")
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .padding(.top, 24)
         }
         .padding()
     }
