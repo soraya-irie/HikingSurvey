2e0c26399d3249b7b6d51fd6cb0a01060da0d301
soraya-ire <rujiangkongshi@gmail.com>
2026-04-22 10:20:31 +0900

    Complete Section4 Step5 - analyze sentiment in text

--- HikingSurvey/Scorer..swift
+++ HikingSurvey/Scorer..swift
@@ -6,5 +6,12 @@ class Scorer {
 
     func score(_ text: String) {
         tagger.string = text
+        tagger.enumerateTags(
+            in: text.startIndex..<text.endIndex,
+            unit: .paragraph,
+            scheme: .sentimentScore,
+            options: []) { _, _ in
+                return false
+            }
     }
 }
