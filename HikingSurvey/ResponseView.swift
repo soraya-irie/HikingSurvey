7c7a541fd17bfb5ad6672dbb03c842635dd07eb8
soraya-ire <rujiangkongshi@gmail.com>
2026-04-22 10:01:10 +0900

    Complete Section3 Step2 - analyze sentiment in text

--- HikingSurvey/ResponseView.swift
+++ HikingSurvey/ResponseView.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 
 struct ResponseView: View {
+    var response: Response
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ResponseView()
+    ResponseView(response: Response(text: "I enjoy hiking very much!"))
 }
