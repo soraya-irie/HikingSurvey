f90c5996f505f883df1fdaf0714eaef63ddaad3d
soraya-ire <rujiangkongshi@gmail.com>
2026-04-22 20:46:22 +0900

    Complete Section9 Step4 - analyze sentiment in text

--- HikingSurvey/ChartView.swift
+++ HikingSurvey/ChartView.swift
@@ -5,7 +5,9 @@ struct ChartView: View {
     var responses: [Response]
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(responses) { response in
+            SectorMark(angle: .value("Type", 1))
+        }
     }
 }
 
