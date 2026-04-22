8caa9265d7f7143f5d690f6869e95d7f68532c56
soraya-ire <rujiangkongshi@gmail.com>
2026-04-22 20:41:00 +0900

    Complete Section9 Step2 - analyze sentiment in text

--- HikingSurvey/ChartView.swift
+++ HikingSurvey/ChartView.swift
@@ -2,11 +2,13 @@ import SwiftUI
 import Charts
 
 struct ChartView: View {
+    var responses: [Response]
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ChartView()
+    ContentView()
 }
