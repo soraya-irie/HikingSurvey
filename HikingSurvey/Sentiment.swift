ff125286aa56ba92d00019da055f72fab4e6ffa3
soraya-ire <rujiangkongshi@gmail.com>
2026-04-22 19:04:00 +0900

    Complete Section6 Step7 - analyze sentiment in text

--- HikingSurvey/Sentiment.swift
+++ HikingSurvey/Sentiment.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 enum Sentiment {
     case positive
@@ -23,6 +24,17 @@ enum Sentiment {
             return "chevron.down.2"
         case .moderate:
             return "minus"
+        }
+    }
+
+    var sentimentColor: Color {
+        switch self {
+        case .positive:
+            return Color(red: 0.99, green: 0.49, blue: 0.00)
+        case .negative:
+            return Color(red: 0.08, green: 0.22, blue: 0.41)
+        case .moderate:
+            return Color(red: 0.00, green: 0.44, blue: 0.49)
         }
     }
 }
